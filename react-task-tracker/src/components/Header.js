9259e39d9b96744ec26c177b4c7e7cde489f4d50
pavel99pop <pavel99pop@gmail.com>
2021-06-26 15:36:32 +0600

    displaying tasks array as task components

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,9 +1,15 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
+import Tasks from './Tasks'
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Click')
+    }
     return (
-        <header>
-            <h1 style={headingStyle}>{title}</h1>
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick} />
         </header>
     )
 }
@@ -16,9 +22,9 @@ Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
 
-const headingStyle = {
-    color: 'red',
-    backgroundColor: 'black'
-}
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black'
+// }
 
 export default Header
