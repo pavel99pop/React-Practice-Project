b0f702c15bcf2d9c7c158535323a22b61e2e0f01
pavel99pop <pavel99pop@gmail.com>
2021-06-26 14:03:43 +0600

    CSS in JS example

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 import Header from './components/Header'
 
-// function App() {
-//   return (
-//     <div className="container">
-//       <Header />
-//     </div>
-//   );
-// }
-
-class App extends React.Component {
-  render() {
-    return <h1>Hello from a class</h1>
-  }
+function App() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
 }
+
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello from a class</h1>
+//   }
+// }
 
 export default App;
